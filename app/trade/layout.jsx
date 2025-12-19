41769ba6bfa223a8162678b7c246d232d6c0b0d8
wealthoceaninstitute-commits <wealthoceaninstitute@gmail.com>
2025-12-19 10:09:29 +0530

    Create layout.jsx

---
+++ app/trade/layout.jsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function TradeLayout({ children }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) router.push("/login");
+  }, []);
+
+  return children;
+}
